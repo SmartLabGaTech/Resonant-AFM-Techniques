f97b628097fadf07e3ec1c44de33549818fb726b
micahterrell <micahterrell@users.noreply.github.com>
2018-01-30 00:22:17 -0500

    Implemented hello world buttons

--- Project/Acquisition/Mediator/Gui.py
+++ Project/Acquisition/Mediator/Gui.py
@@ -1,4 +1,6 @@
 import kivy
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
 kivy.require('1.10.0')
 
@@ -8,7 +10,13 @@ from kivy.uix.label import Label
 class MyApp(App):
 
 	def build(self):
-		return Label(text='Hello world')
+		layout = BoxLayout(orientation='vertical')
+		btn1 = Button(text='Hello')
+		btn2 = Button(text='World')
+		layout.add_widget(btn1)
+		layout.add_widget(btn2)
+
+		return layout
 
 if __name__ == '__main__':
     MyApp().run()
