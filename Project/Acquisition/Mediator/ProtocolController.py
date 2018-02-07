e351d523a45aa322b657163d259aa9dca9e14286
micahterrell <micahterrell@users.noreply.github.com>
2018-02-06 22:07:13 -0500

    Implemented worker threads

--- Project/Acquisition/Mediator/ProtocolController.py
+++ Project/Acquisition/Mediator/ProtocolController.py
@@ -9,10 +9,129 @@ import threading
 import ProtocolServer
 import ProtocolClient
 import socket
+import time
 
 messageQueue = queue.Queue(20)
 
+
+#Note: Each buffer should always contain plain text
+sendBuffer = queue.Queue(20)
+receiveBuffer = queue.Queue(20)
+timeout = 5
+
 ###### Public Functions ######
+
+def runProtocol(sendPort, receivePort):
+	#Start worker threads
+	listenThreadArgs = (receivePort,)
+	listenThread = threading.Thread(target=listenThreadFunction, args=listenThreadArgs)
+	listenThread.daemon = True
+	listenThread.start()
+
+	sendThreadArgs = ('127.0.0.1',sendPort)
+	sendThread = threading.Thread(target=sendThreadFunction, args=sendThreadArgs)
+	sendThread.daemon = True
+	sendThread.start()
+	
+	#Send initial connection message
+	print("Sending connection message to " + str(sendPort))
+	sendConnectMessage()
+
+	if(not connected()):
+		print("Connection not found. Exiting Protocol.")
+		# TODO: Clean up threads
+		return
+
+	print("Connected")
+	return True
+
+def connected():
+	start = time.time()
+	#Wait for timeout seconds for a response
+	while(True):
+		if(time.time() - start >= timeout):
+			print("Connection timed out")
+			return False
+		elif(not receiveBuffer.empty()):
+			#Pop the message and check its type
+			try:
+				message = receiveBuffer.get(False)
+				if(getMessageName(message) == 'connectAck'):
+					return True
+				else:
+					print("Dropped message during connection: " + message)
+				#While connecting, drop all none Ack messages
+			except Empty:
+				pass
+
+#Takes a plain text json message and returns the message name
+def getMessageName(message):
+	return json.loads(message)['message_name']
+
+def sendConnectMessage():
+	sendBuffer.put(buildConnectMessage(), False)
+
+def sendSetParamMessage(parameterName, parameterValue):
+	sendBuffer.put(buildSetParamMessage(parameterName, parameterValue))
+
+def listenThreadFunction(port):
+	host = ''
+
+	# Create the socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.bind((host, port))
+	sock.listen(1)
+
+	while(True):
+		#Wait for a connection
+		connection, clientAddress = sock.accept()
+
+		print("Connection from ", clientAddress)
+
+		while(True):
+			data = connection.recv(1024)
+
+			if(data):
+
+				# Decode the message
+				decodedMessage = data.decode("utf_8")
+				print(decodedMessage)
+				# Buffer the message
+				# Currently if the buffer is full an exception is raised.
+				# TODO: handle dropped messages
+				# TODO: handle exception
+				receiveBuffer.put(decodedMessage, False)		
+
+def sendThreadFunction(host, port):
+
+	
+
+	while(1):
+		#Wait for messages in sendBuffer
+		if(not sendBuffer.empty()):
+			# Create the socket
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			# If there is a message, encode and sent it
+			sock.connect((host, port))
+
+
+			message = sendBuffer.get()
+			print("sent " + message)
+			encodedMessage = message.encode("utf_8")
+			sock.sendall(encodedMessage)
+
+			sock.close()
+		else:
+			# Sleep for a bit
+			time.sleep(.1)
+
+
+
+
+
+
+
+
 
 def connectToLabView(port):
 	print("Connecting on port " + str(port))
@@ -31,10 +150,14 @@ def listen(port):
 ###### Private Functions ######
 
 ##TODO: Document
-def decodeAndQueueMessage(message):
-	decodedMessage = message.decode("utf_8")
-	messageQueue.put(decodedMessage, True, 20)
-	print("TODO: DOC decodeAndQueue: There are ", messageQueue.qsize(), "elements in the q")
+
+
+
+
+
+
+
+
 
 
 #   Builds a BE TCP Control Protocol connect message
